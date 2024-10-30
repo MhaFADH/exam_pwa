1002fd957412d73d79b7b0d4684fcb417fe09c10
Mohamed-Amine FADHLAOUI <fadhl@mohair.local>
2024-10-30 14:43:23 +0100

    fix(atob):remove // from replace -

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,13 @@ import { subscribeUser, unsubscribeUser, sendNotification } from "./actions"
 function urlBase64ToUint8Array(base64String: string) {
   console.log(base64String)
   const padding = "=".repeat((4 - (base64String.length % 4)) % 4)
+  console.log(process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY)
   const base64 = (base64String + padding)
-    .replace(/\\-/g, "+")
+    .replace(/-/g, "+")
     .replace(/_/g, "/")
+    .replace(/\s/g, "")
+
+  console.log(base64)
 
   const rawData = window.atob(base64)
   const outputArray = new Uint8Array(rawData.length)
