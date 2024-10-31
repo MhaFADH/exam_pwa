3b54caa4aeba177261f9a1756ec3b5f6fb1d7bac
zweird <zweir.dd@gmail.com>
2024-10-31 11:02:24 +0100

    feat(job): add job creation link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/button"
 import { ChevronLeft } from "lucide-react"
+import Link from "next/link"
 import { useRouter } from "next/navigation"
 
 const Header = () => {
@@ -10,9 +11,13 @@ const Header = () => {
   const handleBack = () => router.back()
 
   return (
-    <header className="p-4">
+    <header className="p-4 items-center flex justify-between">
       <Button size="icon" variant="outline" onClick={handleBack}>
         <ChevronLeft />
+      </Button>
+
+      <Button variant="outline" asChild>
+        <Link href="/create">Add Job</Link>
       </Button>
     </header>
   )
