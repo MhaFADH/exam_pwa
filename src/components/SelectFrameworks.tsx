5404db02ea9953f511ed1c058b96d326fa42e881
Baptistebsnt <baptiste.boissonnet@gmail.com>
2024-10-31 13:47:23 +0100

    Fix(JobCreationForm): translation

--- src/components/SelectFrameworks.tsx
+++ src/components/SelectFrameworks.tsx
@@ -105,7 +105,7 @@ const SelectFrameworks = ({ onItemsChange }: Props) => {
               ))}
             </div>
           ) : (
-            "Select items..."
+            "Choisissez un tag..."
           )}
         </Button>
       </PopoverTrigger>
